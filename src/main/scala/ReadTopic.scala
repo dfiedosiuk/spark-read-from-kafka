2828412b27764a26f5f4c0fa2807a4bbff700bf4
d.fiedosiuk <dfiedosiuk@adform.com>
2022-04-21 16:19:17 +0200

    Write to kafka format

--- src/main/scala/ReadTopic.scala
+++ src/main/scala/ReadTopic.scala
@@ -1,26 +1,33 @@
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{col, upper}
 import sun.invoke.util.ValueConversions.cast
 
 object ReadTopic extends App {
+
   import org.apache.spark.sql.SparkSession
 
-  val topicName = args(0)
+  val topicInput = args(0)
+  val topicOutput = args(1)
 
   val spark = SparkSession
     .builder
     .master("local[*]")
     .getOrCreate()
 
-  val df  = spark
+  val df = spark
     .readStream
     .format("kafka")
-    .option("subscribe", topicName)
+    .option("subscribe", topicInput)
     .option("kafka.bootstrap.servers", ":9092")
     .load
-    .select(col("value").cast("String"))
+    .withColumn("value", upper(col("value")))
 
   df.writeStream
-    .format("console")
-    .start.awaitTermination()
+    .format("kafka")
+    .option("kafka.bootstrap.servers", ":9092")
+    .option("topic", "output")
+    .option("checkpointLocation", "checkpoint")
+    .start
+    .awaitTermination()
+
 
 }
